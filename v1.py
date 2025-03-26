cf1e53f7e95df3c2a2048d32db269d59a8d4e7cc
Sourav158 <114108771+Sourav158@users.noreply.github.com>
2025-03-26 19:45:10 -0400

    Update v1.py

--- v1.py
+++ v1.py
@@ -12,6 +12,17 @@ LED_PIN_22 = 22  # GPIO 22
 LED_PIN_10 = 10  # GPIO 10
 LED_PIN_9 = 9    # GPIO 9
 
+'''
+distance sensor - non resistor pin gpio 23, w/resistor = 27
+top_light = 22
+bottom_light = 17
+top_water = 13
+bottom_water = 19
+pump = 0
+
+heater = 10
+'''
+
 # Use the correct GPIO chip (Check with ls /dev/gpiochip*)
 chip = gpiod.Chip('gpiochip0')  # Change this if needed
 
@@ -135,7 +146,7 @@ def run_timer_top_light():
    
     total_on = 0
     total_off = 0
-    
+
     while True:
         top_initialized = data.get("top_initialized", False)
         top_mode = data.get("top_mode", "manual")
@@ -216,38 +227,52 @@ def run_timer_top_water():
                     #print(f"Timer One Counter: {counter} seconds")
                 counter = total_off
 
-'''
+
 def manual_mode():
    
-    print("0")
+    level_under_test = data.get("level_under_test", 0)
     print(level_under_test)
+
     if level_under_test == 1:
-        print("1")
+        bottom_initialized = data.get("bottom_initialized", False)
         print(bottom_initialized)
+
         if bottom_initialized == True:
-            print("2")
+            bottom_mode = data.get("bottom_mode", "manual")
             print(bottom_mode)
+
             if bottom_mode == "manual":
+                bottom_man_light = data.get("bottom_man_light", False)
+                bottom_man_water = data.get("bottom_man_water", False)
                 print("3")
+                
                 if bottom_man_light == True:
                     print("4")
                     led_line_17.set_value(1)
                 else: 
                     led_line_17.set_value(0)
+
                 if bottom_man_water == True:
-                    led_line_27.set_value(1)
+                    led_line_22.set_value(1)
                     led_line_9.set_value(1)
                 else:
-                    led_line_27.set_value(0)
+                    led_line_22.set_value(0)
                     led_line_9.set_value(0)
                     
     if level_under_test == 2:
+        top_initialized = data.get("top_initialized", False)
+
         if top_initialized == True:
+            top_mode = data.get("top_mode", "manual")
+
             if top_mode == "manual":
+                top_man_light = data.get("top_man_light", False)
+                top_man_water = data.get("top_man_water", False)
+
                 if top_man_light == True:
-                    led_line_22.set_value(1)
+                    led_line_27.set_value(1)
                 else: 
-                    led_line_22.set_value(0)
+                    led_line_27.set_value(0)
 
                 if top_man_water == True:
                     led_line_10.set_value(1)
@@ -257,7 +282,7 @@ def manual_mode():
                     led_line_9.set_value(0)
     
     time.sleep(2)
-'''
+
             
 # Start fetch_flags() in a separate thread
 #firebase_thread = threading.Thread(target=fetch_flags, daemon=True)
@@ -268,14 +293,14 @@ timer_one_thread = threading.Thread(target=run_timer_bottom_light, daemon=True)
 timer_two_thread = threading.Thread(target=run_timer_bottom_water, daemon=True)
 timer_three_thread = threading.Thread(target=run_timer_top_light, daemon=True)
 timer_four_thread = threading.Thread(target=run_timer_top_water, daemon=True)
-#timer_five_thread = threading.Thread(target=manual_mode, daemon=True)
+timer_five_thread = threading.Thread(target=manual_mode, daemon=True)
 
 # Start both threads
 timer_one_thread.start()
 timer_two_thread.start()
 timer_three_thread.start()
 timer_four_thread.start()
-#timer_five_thread.start()
+timer_five_thread.start()
 
 # Keep the main program running
 while True:
